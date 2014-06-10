8ae41bdc27298520908ea8c1046f8f074c3348d9
Toby Robb <tobyrobb@tobyrobb.com>
2014-06-10 15:17:03 +1000

    Delete publish message _192_168_2_10.bat

--- publish message _192_168_2_10.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p input="Write something, it will be sent to the DMD  ...      "
-echo "Sending mosquitto message"
-cd C:\Program Files (x86)\mosquitto
-mosquitto_pub.exe -h 192.168.2.10 -t DMD -m  "%input%"
-echo "Sent mosquitto message"
